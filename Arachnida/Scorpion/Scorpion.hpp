ec745547fa9c710b414e262a04d5607ed18d6076
furkann-ekz <fekiz@student.42istanbul.com.tr>
2025-12-28 18:41:49 +0300

    Scorpion started

---
+++ Arachnida/Scorpion/Scorpion.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Scorpion.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fekiz <fekiz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/28 18:22:16 by fekiz             #+#    #+#             */
+/*   Updated: 2025/12/28 18:41:11 by fekiz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCORPION_HPP
+#define SCORPION_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <arpa/inet.h>
+#include <algorithm>
+#include <sstream>
+
+class Scorpion
+{
+	private:
+		std::vector<unsigned char>	data;
+	public:
+};
+
+#endif
